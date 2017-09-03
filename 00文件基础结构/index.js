f1a0a1a6b03ecc55c632e2bef9dc764297c01cc7
visugar <visugar@163.com>
2017-09-03 08:51:19 +0800

    modified 00file about common js

--- 00文件基础结构/index.js
+++ 00文件基础结构/index.js
@@ -1,80 +1,158 @@
 /*下面是常用的封装好的几个方法*/
 
-/*发送请求*/
-function ajax(url, success, fail) {
+/*发送ajax请求*/
+function ajax(url, data, method, success, error) {
     var xhr = null;
     if (window.XMLHttpRequest) {
         xhr = new XMLHttpRequest();
     } else {
         xhr = new ActiveXObject("Microsoft.XMLHTTP");
     }
-    xhr.open("GET", url, false);
+
+    var resA = '',
+        data = data || {},
+        method = method || 'get',
+        success = success || function () {},
+        error = error || function (err) {
+                alert(url + '请求发生错误！');
+            };
+
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4) {
-            if ((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304) {
-                success(xhr.responseText);
+            if (xhr.status >= 200 && xhr.status < 300 || xhr.status == 304 || xhr.status == 0) {
+                success && success(xhr.responseText);
             } else {
-                fail(xhr.status);
+                error && error(xhr.status);
             }
         }
+    };
+
+    if (data) {
+        var res = [];
+        for (var i in data) {
+            res.push(encodeURIComponent(i) + '=' + encodeURIComponent(data[i]));
+        }
+        resA = res.join('&');
+    }
+
+    if (method === 'get') {
+        if (data) {
+            url += '?' + resA;
+        }
+        xhr.open(method, url, true);
+        xhr.send(null);
+    }
+
+    if (method === 'post') {
+        xhr.setRequestHeader('Content-type', "application/x-www-form-urlencoded");
+        xhr.open(method, url, true);
+        xhr.send(resA);
     }
 }
+
 
 /*绑定事件*/
-function addEvent(element, type, handler) {
-    if (element.addEventListener) {
-        element.addEventListener(type, handler, false);
-    } else if (element.attachEvent) {
-        element.attachEvent("on" + type, handler);
+function addLister(el, type, handler) {
+    if (el.addEventListener) {
+        el.addEventListener(type, handler, false);
+    } else if (el.attachEvent) {
+        el.attachEvent('on' + type, handler);
     } else {
-        element["on" + type] = handler;
+        el['on' + type] = handler;
     }
 }
 
-/*添加class*/
-function addClass(element, newClass) {
-    var className = element.className;
-    if (!className) {
-        element.className = newClass;
-    } else {
-        if (className.indexOf(newClass) === -1) {
-            className = className + " ";
-            className += newClass;
-            element.className = className;
-        }
-    }
-
-}
-
-/*移除class*/
-function removeClass(element, newClass) {
-    var className = element.className;
-    if (className.indexOf(newClass) > -1) {
-        var reg = new RegExp("\s*" + newClass + "\s*", "gi");
-        className = className.replace(reg, " ");
-        element.className = className;
-    }
-}
-
-/*查找是否有该class*/
-function hasClass(element, cls) {
-    var className = element.className;
-    if (className.indexOf(cls) > -1) {
+/*判断是否有className*/
+function hasClass(el, className) {
+    var cls = el.className;
+    if (cls.indexOf(className) > -1) {
         return true;
     }
     return false;
 }
 
+/*移除className*/
+function removeClass(el, className) {
+    var cls = el.className.split(/\s+/);
+    if (!cls[0]) return;
+    for (var i = 0; i < cls.length; i++) {
+        if (cls[i] == className) {
+            cls.splice(i, 1);
+            el.className = cls.join(' ');
+        }
+    }
+}
+
+/*添加className*/
+function addClass(el, className) {
+    var cls = el.className.split(/\s+/);
+    if (!cls[0]) {
+        el.className = className;
+    } else {
+        el.className += " " + className;
+    }
+}
+
 /*按id获取该元素*/
-function $(id) {
-    return document.getElementById(id);
+function $(id, node) {
+    return (node || document).getElementById(id);
+}
+
+/*按className来获取该元素*/
+function getEleByClass(className, node) {
+    return (node || document).getElementsByClassName(className);
 }
 
 /*获取样式*/
-function getStyle(element) {
+function getStyle(el) {
     if (window.getComputedStyle) {
-        return window.getComputedStyle(element);
-    } else if (element.currentStyle) {
-        return element.currentStyle;
+        return window.getComputedStyle(el);
+    } else if (el.currentStyle) {
+        return el.currentStyle;
+    }
+}
+
+/*设置cookie*/
+function setCookie(name, val, days) {
+    var cookie = encodeURIComponent(name) + '=' + encodeURIComponent(val);
+    var exp = new Date();
+    exp.setTime(exp.getTime() + days * 24 * 60 * 60 * 1000);
+    cookie += '; expires = ' + exp.toGMTString();
+    document.cookie = cookie;
+
+}
+
+/*获取cookie值*/
+function getCookie() {
+    var cookie = {};
+    var all = document.cookie;
+
+    if (all == '') {
+        return cookie;
+    }
+    var list = all.split('; ');
+    for (var i = 0; i < list.length; i++) {
+        var item = list[i];
+        var p = item.indexOf('=');
+        var name = item.substring(0, p);
+        name = decodeURIComponent(name);
+        var value = item.substring(p + 1);
+        value = decodeURIComponent(value);
+        cookie[name] = value;
+    }
+    console.log(cookie);
+    return cookie;
+}
+
+/*load相关事件*/
+function addLoadEvent(func) {
+    var loadFunc = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function () {
+            loadFunc();
+            func();
+        }
     }
 }
